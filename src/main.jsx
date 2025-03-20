2201cea862d4f3b81cf8fb554300b47a728d03df
Dmitry Likane <dmitri.likane@connectid.com.au>
2025-03-20 22:06:55 +1100

    Don't merge, try to mve themes out. Doesn't work.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css'; // ✅ Import global styles
 import App from './App.jsx';
+import "./styles/themeNight.css";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
